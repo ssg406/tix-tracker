80cb6a382c4ef634e43d069526b945adab5c5ca7
Sam Jones <88912351+ssg406@users.noreply.github.com>
2022-11-09 00:30:23 -0800

    Delete and Patch routes implemented

--- server/test/tickets.test.js
+++ server/test/tickets.test.js
@@ -9,6 +9,18 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+/**
+ * Objects to send during testing
+ */
+const testUser = {
+  email: "testemail@gmail.com",
+  password: "Compl!!!aNT**Passw000rd",
+  name: "Test Name",
+  role: "user",
+};
+
+const validObjectId = "6369ff8ba505b65f05ce2343";
+
 const validTicket = {
   date: new Date(),
   description: "A description of a ticket",
@@ -26,39 +38,46 @@ describe("Ticket Routes at /api/v1/tickets", () => {
       done();
     });
   });
+
   describe("/tickets GET all tickets", () => {
+    beforeEach((done) => {
+      const testTicket = Ticket.create(validTicket);
+      testTicket.save().then(() => done());
+    });
     it("All tickets should be returned", (done) => {
       chai
         .request(app)
-        .get("/all")
+        .get("api/v1/tickets/all")
         .end((err, res) => {
           res.should.have.status(StatusCodes.OK);
           res.body.should.be.a("array");
-          res.body.length.should.be.eql(0);
+          res.body.length.should.be.eql(1);
           done();
         });
     });
   });
+
   describe("/newTicket POST create new ticket", () => {
     it("Valid ticket should be created and returned with ID", (done) => {
       chai
         .request(app)
-        .post("/new")
+        .post("api/v1/tickets/new")
         .send(validTicket)
         .end((err, res) => {
           res.should.have.status(StatusCodes.CREATED);
           res.body.should.be.a("object");
-          res.body.should.have.property("_id");
-          res.body.should.have.property("status");
-          res.body.should.have.property("user");
-          res.body.status.should.eql("open");
+          res.body.should.have.property("ticket");
+          res.body.ticket.should.have.property("_id");
+          res.body.ticket.should.have.property("status");
+          res.body.ticket.should.have.property("createdBy");
+          res.body.ticket.status.should.eql("open");
           done();
         });
     });
     it("Ticket missing date should be rejected with error message", (done) => {
       chai
         .request(app)
-        .post("/new")
+        .post("api/v1/tickets/new")
         .send(ticketNoDate)
         .end((err, res) => {
           res.should.have.status(StatusCodes.BAD_REQUEST);
@@ -71,30 +90,91 @@ describe("Ticket Routes at /api/v1/tickets", () => {
     it("Ticket missing description should be rejected with error message", (done) => {
       chai
         .request(app)
-        .post("/new")
+        .post("api/v1/tickets/new")
         .send(ticketNoDescription)
         .end((err, res) => {
           res.should.have.status(StatusCodes.BAD_REQUEST);
-          res.body.should.be("object");
+          res.body.should.be.a("object");
           res.body.should.have.property("fields");
           res.body.fields.should.eql(["description"]);
           done();
         });
     });
   });
+
   describe("/updateTicket PATCH modify ticket", () => {
     it("Updated ticket should be accepted and details returned", (done) => {
       chai
         .request(app)
-        .post("/update")
-        .send({})
+        .post("api/v1/tickets/update")
+        .send({ ...validTicket, description: "New Description" })
         .end((err, res) => {
-          // test body
+          res.should.have.status(StatusCodes.OK);
+          res.body.should.be.a("object");
+          res.body.should.have.property("ticket");
+          res.body.ticket.should.have.property("_id");
+          res.body.ticket.should.have.property("status");
+          res.body.ticket.should.have.property("createdBy");
+          res.body.ticket.should.have.property("description");
+          res.body.ticket.description.should.eql("New Description");
+          done();
         });
     });
-    it("");
   });
+
   describe("/deleteTicket DELETE deletes ticket", () => {
-    it("Ticket should be removed if correct details given", (done) => {});
+    beforeEach((dont) => {
+      const testTicket = new Ticket(validTicket);
+      testTicket.save().then(() => done());
+    });
+    it("Ticket should be removed if correct details given", (done) => {
+      const ticket = Ticket.findOne({
+        date: testTicket.date,
+        description: testTicket.description,
+      });
+      const { _id } = ticket;
+      chai
+        .request(app)
+        .delete("api/v1/tickets/delete")
+        .send({ _id })
+        .end((err, res) => {
+          res.body.should.have.status(StatusCodes.OK);
+          res.body.should.be.a("object");
+          res.body.should.have.property("_id");
+          res.body.should.have.property("message");
+          res.body.message.should.eql("Ticket deleted");
+          done();
+        });
+    });
+    it("Not found message should be sent if no ticket exists", (done) => {
+      chai
+        .request(app)
+        .delete("api/v1/tickets/delete")
+        .send({ _id: validObjectId })
+        .end((err, res) => {
+          res.body.should.be.a("object");
+          res.status.should.be(StatusCodes.NOT_FOUND);
+          res.body.should.have.property("message");
+          res.body.message.should.eql("Could not find a ticket with that ID");
+          done();
+        });
+    });
+    it("Error mesage should be sent if invalid ID is given", (done) => {
+      chai
+        .request(app)
+        .delete("api/v1/tickets/delete")
+        .send("Invalid ID")
+        .end((err, res) => {
+          res.status.should.be(StatusCodes.BAD_REQUEST);
+          res.body.should.be.a("object");
+          res.body.should.have.property("fields");
+          res.body.fields.should.eql(["_id"]);
+          res.body.should.have.property("messages");
+          res.body.messages.should.eql(
+            "Field is invalid for the given request"
+          );
+          done();
+        });
+    });
   });
 });
---
+++ server/util/testData.js
@@ -0,0 +1,104 @@
+/**
+ * Test User Data
+ */
+[
+  {
+    email: "apaxton0@issuu.com",
+    name: "Aldridge Paxton",
+    password: "dlq75Ulg17",
+  },
+  {
+    email: "npickthorn1@nbcnews.com",
+    name: "Nessa Pickthorn",
+    password: "KSZhOl",
+  },
+  {
+    email: "aorbine2@va.gov",
+    name: "Angel Orbine",
+    password: "qx27GJY",
+  },
+  {
+    email: "frayer3@dot.gov",
+    name: "Freedman Rayer",
+    password: "lfa35yYZiY",
+  },
+  {
+    email: "ccritcher4@geocities.jp",
+    name: "Chico Critcher",
+    password: "fdVlXm5K",
+  },
+][
+  /**
+   * Test Ticket Data
+   */
+  ({
+    date: "2022/09/17",
+    description: "tempus vel pede morbi porttitor",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf0",
+    },
+  },
+  {
+    date: "2022/11/28",
+    description: "mauris lacinia sapien quis libero",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf1",
+    },
+  },
+  {
+    date: "2022/09/14",
+    description: "ornare imperdiet sapien urna pretium",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf2",
+    },
+  },
+  {
+    date: "2022/09/23",
+    description: "neque sapien placerat ante nulla",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf3",
+    },
+  },
+  {
+    date: "2022/01/20",
+    description: "orci eget orci vehicula condimentum",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf4",
+    },
+  },
+  {
+    date: "2022/02/24",
+    description: "vestibulum sed magna at nunc commodo",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf5",
+    },
+  },
+  {
+    date: "2022/01/15",
+    description: "nec euismod scelerisque quam turpis adipiscing",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf6",
+    },
+  },
+  {
+    date: "2022/07/11",
+    description: "non quam nec dui luctus rutrum nulla tellus",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf7",
+    },
+  },
+  {
+    date: "2022/04/22",
+    description: "nisi nam ultrices libero non mattis pulvinar nulla",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf8",
+    },
+  },
+  {
+    date: "2022/09/23",
+    description: "lorem ipsum dolor sit amet consectetuer",
+    createdBy: {
+      $oid: "636b571dfc13ae3f12000bf9",
+    },
+  })
+];
