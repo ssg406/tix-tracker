d24acde07418b176c4d01ac1b7f48e3d7cb9bbc4
Sam Jones <88912351+ssg406@users.noreply.github.com>
2022-12-14 21:43:45 -0800

    Bug fix in error handler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,11 +1,11 @@
 import StatusCodes from 'http-status-codes';
 
 const errorHandler = (err, req, res, next) => {
+  // Build the error from given object or populate defaults
   const thrownError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || 'Internal server error. Please wait and try again.',
   };
-
   // Check for mongoose ValidationErrors
   if (err.name === 'ValidationError') {
     // Get field and message for each validation error
@@ -19,17 +19,20 @@ const errorHandler = (err, req, res, next) => {
   else if (err.name === 'CastError') {
     const field = err.path;
     const messages = `${field} is invalid for the given request`;
-    res.status(StatusCodes.BAD_REQUEST).json({ fields, message });
+    res.status(StatusCodes.BAD_REQUEST).json({ field, messages });
   }
   // Check for mongoose duplicate of unique key error
   else if (err.code && err.code === 11000) {
     const fields = Object.keys(err.keyValue);
     const message = `An account with the specified ${fields} already exists`;
     res.status(StatusCodes.CONFLICT).json({ fields, message });
-    // Return general error
-  } else if (err.name === 'UnauthorizedError') {
+  }
+  // Error thrown by express-jwt for invalid/missing tokens
+  else if (err.name === 'UnauthorizedError') {
     res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid token' });
-  } else {
+  }
+  // Return error that was thrown initially with given status and message
+  else {
     res.status(thrownError.statusCode).json({ message: thrownError.message });
   }
 };
